4be6e54c2f2586d00b5da279be291f5f3263d04b
Harrold Paderan <harrold_paderan@yahoo.com>
2018-01-31 23:40:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/harrold/myapplication/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.harrold.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView allSubsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        allSubsList = (ListView) findViewById(R.id.allSubsList);
+        Button addButton = (Button) findViewById(R.id.AddButton);
+        Button editButton = (Button) findViewById(R.id.EditButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+
+                /*
+                String text = bodyText.getText().toString();
+                saveInFile(text, new Date(System.currentTimeMillis()));
+                */
+
+                finish();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        /*
+        String[] tweets = loadFromFile();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.list_item, tweets);
+        allSubsList.setAdapter(adapter);
+        */
+
+    }
+
+    private String[] loadFromFile() {
+        /*
+        ArrayList<String> tweets = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = in.readLine();
+            while (line != null) {
+                tweets.add(line);
+                line = in.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return tweets.toArray(new String[tweets.size()]);
+        */
+
+        //remember to take this off
+        return null;
+
+    }
+
+    private void saveInFile(String text, Date date) {
+        /*
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_APPEND);
+            fos.write(new String(date.toString() + " | " + text)
+                    .getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        */
+    }
+}
